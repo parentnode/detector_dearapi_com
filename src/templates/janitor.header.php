09b0cf5a7e8a2cc332a4e2f32ed8e81651040f52
kaestel <martin@think.dk>
2015-07-19 18:23:27 +0200

    extended servicenavigation

--- src/templates/janitor.header.php
+++ src/templates/janitor.header.php
@@ -28,7 +28,13 @@
 <div id="page" class="i:page">
 	<div id="header">
 		<ul class="servicenavigation">
-			<li class="keynav front"><a href="/janitor">Devices Admin</a></li>
+			<li class="keynav front"><a href="/janitor">Janitor</a></li>
+<?			if(session()->value("user_id") && session()->value("user_group_id") > 1): ?>
+			<li class="keynav web nofollow"><a href="/"><?= SITE_NAME ?></a></li>
+			<li class="keynav user nofollow"><a href="?logoff=true">Logoff</a></li>
+<?			else: ?>
+			<li class="keynav user nofollow"><a href="/login">Login</a></li>
+<?			endif; ?>
 		</ul>
 	</div>
 
