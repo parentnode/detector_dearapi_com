19551c83c5205ce179ebcba270dbfe98441f8788
kaestel <martin@think.dk>
2015-07-20 01:15:28 +0200

    Fixed php warning

--- src/www/jsonp/index.php
+++ src/www/jsonp/index.php
@@ -9,7 +9,7 @@ include_once("classes/identify.class.php");
 
 $action = $page->actions();
 
-$ua = stringOr(getVar("ua"), $_SERVER["HTTP_USER_AGENT"]);
+$ua = stringOr(getVar("ua"), isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "Not accessible");
 $callback = stringOr(getVar("callback"), "callback");
 
 $Identify = new Identify();
