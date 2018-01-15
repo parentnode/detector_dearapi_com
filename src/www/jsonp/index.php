8a871582ca3d982d36f50fe3c7d87eea0ea8e036
Martin Kæstel Nielsen <martin@think.dk>
2018-01-15 17:49:35 +0000

    stop empty UA test

--- src/www/jsonp/index.php
+++ src/www/jsonp/index.php
@@ -9,7 +9,7 @@ include_once("classes/identify.class.php");
 
 $action = $page->actions();
 
-$ua = stringOr(getVar("ua"), isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "UA not available");
+$ua = stringOr(getVar("ua"), isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "");
 $callback = stringOr(getVar("callback"), "callback");
 
 $Identify = new Identify();
