9b7971a6f9cc04e2e52f5b18482a0754ee6f18ed
Martin Kæstel Nielsen <martin@think.dk>
2018-06-17 22:09:21 +0200

    Correct syntax

--- src/config/db/unidentified_useragents.sql
+++ src/config/db/unidentified_useragents.sql
@@ -2,10 +2,10 @@ CREATE TABLE `SITE_DB`.`unidentified_useragents` (
   `id` int(11) NOT NULL auto_increment,
 
   `useragent` text NOT NULL,
-  `comment` text,
+  `comment` text NOT NULL DEFAULT '',
 
-  `identified_as` varchar(11) NOT NULL,
-  `identified_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `identified_as` varchar(30) NOT NULL DEFAULT '',
+  `identified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
