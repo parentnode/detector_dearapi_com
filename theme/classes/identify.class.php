018b230c6f3afbcc3b4cd07a85d5144c0f25db30
kaestel <martin@think.dk>
2023-08-07 22:52:42 +0200

    php8 fix

--- theme/classes/identify.class.php
+++ theme/classes/identify.class.php
@@ -852,7 +852,7 @@ class Identify {
 
 			// remove agent parts from behind
 			$partial_useragent = $useragent;
-			while($partial_useragent = implode(explode("/", $partial_useragent, -1), "/")) {
+			while($partial_useragent = implode("/", explode("/", $partial_useragent, -1))) {
 				$sql = "SELECT item_id FROM ".$this->db_useragents." WHERE useragent LIKE '%$partial_useragent%'";
 				if($query->sql($sql)) {
 					$device_id = $query->result(0, "item_id");
